1cc7483e2cd1af7e1c27081aa7dedfbaef448294
surajsah2053 <surajshah2053@gmail.com>
2020-07-12 19:42:14 +0545

    assignment 3 added

--- Python/Assignment 2/Question_19.py
+++ Python/Assignment 2/Question_19.py
@@ -1,3 +1,17 @@
 """Write a Python class to find validity of a string of parentheses, '(',
 ')', '{', '}', '[' and ']. These brackets must be close in the correct order,
 for example "()" and "()[]{}" are valid but "[)", "({[)]" and "{{{" are invalid"""
+globvar = 0
+
+
+def set_globvar_to_one():
+    global globvar
+    globvar = 1
+
+
+def print_globvar():
+    print(globvar)
+
+
+set_globvar_to_one()
+print_globvar()
